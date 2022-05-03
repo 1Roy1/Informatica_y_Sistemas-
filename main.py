7afa433cb3e6b86fc6ae6db3930925ef430cb922
Roí Rosales <rosalesroy11@gmail.com>
2022-05-03 09:38:33 -0600

    Main
    Se creo un archivo de python

---
+++ main.py
@@ -0,0 +1,9 @@
+print("----------UNIVERSIDAD RAFAEL LANDIVAR----------")
+print("---Estudiante: Roí Eleazar Rosales Arriola---")
+print("---Carné: Roí Eleazar Rosales Arriola---")
+print("---Carrera: Ingeniería en Informatica y Sistemas---")
+print("---Ciclo: Primer Ciclo 2022---")
+print("Curso: Introducción a la Informatica y Sistemas")
+input("Presione Enter para continuar\n")
+
+print("¡¡¡¡BIENVENIDO USUARIO, ¿DESCUBRIRAS QUIEN ES MI ANIMAL FAVORITO?!!!\n")
